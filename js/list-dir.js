79acd90336b6f97439b5421424b5a4f699adf3bf
ZHAO WENLONG <zhaowenlong@Raymonds-MacBook.local>
2024-01-18 12:14:04 +0800

    update

--- js/list-dir.js
+++ js/list-dir.js
@@ -1,6 +1,15 @@
 import fs from 'node:fs'
 
-const listContents = (err, files) => {}
+const listContents = (err, files) => {
+    console.log('running callback')
+    if (err) {
+        console.error(err)
+    } else {
+        for (const name of files) {
+            console.log(name)
+        }
+    }
+}
 const srcDir = process.argv[2]
 const results = fs.readdir(srcDir, listContents)
 console.log('last line of program')
