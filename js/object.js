96807a66039eedfa7269dfdee13f5096bf6e6f89
ZHAO WENLONG <zhaowenlong@Raymonds-MacBook.local>
2024-03-11 21:13:49 +0800

    update

---
+++ js/object.js
@@ -0,0 +1,17 @@
+//ch4
+// an object is a container of properies
+
+const main = () => {
+
+    var empty_object = {}
+
+    var stooge = {
+        "first-name": "Jerome",
+        "last-name": "Howard"
+    }
+
+    console.log(stooge["first-name"])
+
+}
+
+main()
--- js/unittest.js
+++ js/unittest.js
@@ -47,7 +47,7 @@ const sign = (value) => {
 }
 
 hopeThat('Sign of negative is -1', () => assert(sign(-3) === -1))
-hopeThat('Sign of zero is 1', () => assert(sign(19) === 1))
+hopeThat('Sign of zero is 0', () => assert(sign(0) === 0))
 hopeThat('Sign misspelled is error', () => assert(sgn(1) === 1))
 
 main()
